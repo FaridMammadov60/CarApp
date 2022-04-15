d437c798c8b848cca065adc781f9e0e78809b041
FaridMammadov60 <faridmma@code.edu.az>
2022-04-15 04:05:56 -0700

    add: IRepository interface create

---
+++ CarApp/DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    internal interface IRepository<T> where T : IEntity
+    {
+        bool Create(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        T GetOne(Predicate<T> filter = null);
+        List<T> GetAll(Predicate<T> filter = null);
+    }
+}
