3d92dedea9ab002f3367b156362143f01f7cae99
FaridMammadov60 <faridmma@code.edu.az>
2022-04-15 03:45:37 -0700

    add: create Entity library(create brand and model class and IEntity interface

---
+++ CarApp/Entities/Models/Brand.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Brand:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Model> Model { get; set; }
+
+        public Brand()
+        {
+            Model = new List<Model>();
+        }
+    }
+}
---
+++ CarApp/Entities/Models/Model.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Model:IEntity
+    {
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Production { get; set; }
+        public string Color { get; set; }
+        public int Mph { get; set; }
+        
+        
+    }
+}
